8e3ee6f7c607a28d9018b3c856aa986708cfb5f4
mathix <mathixx@gmail.com>
2018-03-16 11:32:16 +0100

    Making call using axios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('aasadasd');
+import axios from 'axios';
+import * as https from 'https';
+
+async function main(){
+    let resp = await axios.get('https://external.loki.thunderpick.com', {
+        auth: {
+            username: 'x',
+            password: 'x'
+        },
+        httpsAgent: new https.Agent({ rejectUnauthorized: false}),
+    });
+
+    console.log(resp.data);
+}
+
+main();
